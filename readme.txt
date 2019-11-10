a179f22634c57fcfc98b95955fe191c3374b37ba
Ryan Pili <rpili@ucsc.edu>
2019-11-09 22:03:16 -0800

    corrected instr2 for director and matcher, updated readme.

--- readme.txt
+++ readme.txt
@@ -1,3 +1,10 @@
+This is a set of psychopy based experiments based on Clark & Gibbs's tangram task.
+tg_main_set1.py and tg_main_set2.py are to be presented to the matcher.
+tg_dr_set1.py and tg_dr_set2.py are to be presented to the director. 
+The experiment presents six tangram matching trials.
+There are 2 sets of 12 tangrams. The "set1" and "set2" denote which set is presented first.
+Between the two sets, a TIPI test is presented to determine impressions of the conversational partner.
+
 20191105 - ryan pili
 tg_main_set1.py, tg_main_set2.py are good to go
     just need to remove the easy_succ
--- tg_main_set1.py
+++ tg_main_set1.py
@@ -125,7 +125,7 @@ text_2 = visual.TextStim(win=win, name='text_2',
 # Initialize components for Routine "instr2"
 instr2Clock = core.Clock()
 text = visual.TextStim(win=win, name='text',
-    text='Here is a video example.',
+    text='On the next slide you will see 12 tangrams in an incorrect order. \n \nYour goal is to work with your partner to move your tangrams to match their correct order. \n \nOn each trial, do not press continue until your partner is ready to continue. \n \nClick anywhere to begin the first trial out of three.',
     font='Arial',
     pos=(0, 0), height=0.05, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1, 
