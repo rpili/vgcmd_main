2c43f633f2124bd1afaf4a2eb51f7db2d87c5893
[ryanEOS] <[rjpili@live.com]>
2019-11-23 17:38:52 -0800

    created data frame in tg_transcript_parser.py, and am working on creating graphs.

--- analysis/verbal/tg_transcript_parser.py
+++ analysis/verbal/tg_transcript_parser.py
@@ -5,6 +5,8 @@
 
 import pathlib 
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 from loguru import logger as log
 
 # log.disable(log.DEBUG)
@@ -48,16 +50,42 @@ def bc_counter(words: list)->int:
     return bc_count
 
 
+def word_counter(av_list: list, ao_list: list):
+    av_wc = [len(word_list) for word_list in av_list] 
+    ao_wc = [len(word_list) for word_list in ao_list]
+    return av_wc, ao_wc
+ 
+
+def count_framer(ao_bcs: list, av_bcs: list, av_wc: list, ao_wc: list):
+    counts_dict = {"av_bc":av_bcs, "ao_bc":ao_bcs, "av_wc":av_wc, "ao_wc":ao_wc}
+    counts_df = pd.DataFrame.from_dict(counts_dict)
+    return counts_df
+
+
+# turn the .txt files in to list of lists of words
+# each list in the list is from one dyad, len(list) = number of dyads
+# with no [ ] or :
 av_list, ao_list = transcript_parse()
 
+
+# for each list of words, go through it and check all the *
+# return a list of backchannel counts for each dyad, for each condition
 ao_bcs = [bc_counter(item) for item in ao_list]
 av_bcs = [bc_counter(item) for item in av_list]
 
 log.debug(("ao:", ao_bcs, "av", av_bcs))
 
-print("av word count is", len(av_list[0])) 
-print("ao word count is", len(ao_list[0])) 
+# count all the words in each condition by feeding in the list of lists of words
+# returns a list of counts
+av_wc, ao_wc = word_counter(av_list, ao_list) 
+print(f"av word counts are {av_wc}, and ao_wc counts are {ao_wc}")
 
+# turn these counts into a pandas df so we can graph nicely
+counts_df = count_framer(ao_bcs, av_bcs, ao_wc, av_wc)
+print(counts_df)
 
-
+# counts_df = counts_df.reset_index()
+# print(counts_df)
+sns.boxplot(data=counts_df)
+plt.show()
 
